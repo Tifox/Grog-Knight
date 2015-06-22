0fe24c55daacf0be03e081fca90bb057fd98155a
Ne02ptzero <louis@ne02ptzero.me>
2015-06-22 14:03:41 +0200

    Fix(OSX): Fix some compilation warnings

--- Angel/Libraries/glfw-3.0.3/deps/GL/glext.h
+++ Angel/Libraries/glfw-3.0.3/deps/GL/glext.h
@@ -4130,8 +4130,8 @@ GLAPI void APIENTRY glVertexBlendARB (GLint count);
 
 #ifndef GL_ARB_vertex_buffer_object
 #define GL_ARB_vertex_buffer_object 1
-typedef ptrdiff_t GLsizeiptrARB;
-typedef ptrdiff_t GLintptrARB;
+//typedef ptrdiff_t GLsizeiptrARB;
+//typedef ptrdiff_t GLintptrARB;
 #define GL_BUFFER_SIZE_ARB                0x8764
 #define GL_BUFFER_USAGE_ARB               0x8765
 #define GL_ARRAY_BUFFER_ARB               0x8892

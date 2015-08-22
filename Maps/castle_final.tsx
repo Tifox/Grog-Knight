c91f35e47bedef265020a1ab8c3dc73d97c40eb0
N0ich <vrey@student.42.fr>
2015-08-22 19:03:43 +0200

    Add(Work on the final tsx)

---
+++ Maps/castle_final.tsx
@@ -0,0 +1,350 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle_final" tilewidth="32" tileheight="32" tilecount="216">
+ <image source="castle.png" width="288" height="768"/>
+ <tile id="27">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="hitbox" value="lowerHalfHitbox"/>
+   <property name="speType" value="spikes"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="hitbox" value="lowerHalfHitbox"/>
+   <property name="speType" value="spikes"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="physic" value="0"/>
+   <property name="speType" value="water"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="physic" value="0"/>
+   <property name="speType" value="water"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="physic" value="0"/>
+   <property name="speType" value="water"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="hitbox" value="upperTierHitbox"/>
+   <property name="speType" value="canCross"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="hitbox" value="rightHalfHitbox"/>
+   <property name="speType" value="spikes"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="enemyLevel" value="-1"/>
+   <property name="physic" value="0"/>
+   <property name="spawnEnemy" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="enemyLevel" value="0"/>
+   <property name="physic" value="0"/>
+   <property name="spawnEnemy" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="enemyLevel" value="+1"/>
+   <property name="physic" value="0"/>
+   <property name="spawnEnemy" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="physic" value="0"/>
+   <property name="spawnDealer" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="hitbox" value="leftHalfHitbox"/>
+   <property name="speType" value="spikes"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="enemyLevel" value="-1"/>
+   <property name="isFlying" value="true"/>
+   <property name="physic" value="0"/>
+   <property name="spawnEnemy" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="enemyLevel" value="0"/>
+   <property name="isFlying" value="true"/>
+   <property name="physic" value="0"/>
+   <property name="spawnEnemy" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="enemyLevel" value="+1"/>
+   <property name="isFlying" value="true"/>
+   <property name="physic" value="0"/>
+   <property name="spawnEnemy" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="physic" value="0"/>
+   <property name="spawnShop" value="true"/>
+   <property name="transparency" value="100"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="hitbox" value="upperHalfHitbox"/>
+   <property name="speType" value="spikes"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="126">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+ <tile id="127">
+  <properties>
+   <property name="physic" value="0"/>
+  </properties>
+ </tile>
+</tileset>

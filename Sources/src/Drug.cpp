5e73032a591ab1f302acc08c4001107e373b9ff7
Fakoc <manon.budin@gmail.com>
2015-09-08 14:38:28 +0200

    Fix(drug): effect of the morphone ok

--- Sources/src/Drug.cpp
+++ Sources/src/Drug.cpp
@@ -167,8 +167,7 @@ void			Drug::pot(int status) {
 	} else if(status == 0) {
 		Game::getHUD()->setText(":(", hero, Vector3(255, 0, 0), 1, 0);
 		hero->buff.bonusDmg = -(hero->getWeapon()->getDamage() / getEffect());
-	}
-	else if(status == 2) {
+	} else if(status == 2) {
 		hero->buff.bonusDmg = 0;
 	}
 }
@@ -187,8 +186,7 @@ void			Drug::cocaine(int status) {
 	} else if(status == 0) {
 		Game::getHUD()->setText(":(", hero, Vector3(255, 0, 0), 1, 0);
 		hero->buff.bonusSpeed = -(hero->_getAttr("forward", "force").asInt() / getEffect());
-		}
-	else if(status == 2) {
+	} else if(status == 2) {
 		hero->buff.bonusSpeed = 0;
 	}
 }
@@ -196,22 +194,19 @@ void			Drug::cocaine(int status) {
 
 void			Drug::morphine(int status) {
 	Characters *hero = Game::currentGame->getHero();
-	std::cout << "EFFECT ==== > " << this->getEffect() << std::endl;
 
 	if (status == 1) {
 		Game::getHUD()->setText(":)", hero, Vector3(0, 255, 0), 1, 0);
 		hero->setInvincibility(true);
-	std::cout << "EFFECT ==== > " << this->getEffect() << std::endl;
 		this->_curDrug = "morphine";
 		theSwitchboard.SubscribeTo(this, "endBonus");
 		theSwitchboard.SubscribeTo(this, "endMalus");
-		theSwitchboard.DeferredBroadcast(new Message("endBonus"), 10);
-		theSwitchboard.DeferredBroadcast(new Message("endMalus"), 30);
+		theSwitchboard.DeferredBroadcast(new Message("endBonus"), 20);
+		theSwitchboard.DeferredBroadcast(new Message("endMalus"), 20);
 	} else if(status == 0) {
 		Game::getHUD()->setText(":(", hero, Vector3(255, 0, 0), 1, 0);
 		hero->setHP(getEffect());
-		}
-	else if(status == 2) {
+	} else if(status == 2) {
 		hero->setInvincibility(false);
 	}
 }
